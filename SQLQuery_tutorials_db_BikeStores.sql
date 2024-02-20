6e2aa0c1ae5f567fa2bc541c9464c0242e0f8cf6
Muhammad Shihab Uddin <shihab.apps.developer@gmail.com>
2024-02-20 18:06:44 +0800

    reformat

--- SQLQuery_tutorials_db_BikeStores.sql
+++ SQLQuery_tutorials_db_BikeStores.sql
@@ -15,9 +15,10 @@ SELECT
 	min(list_price) as min_price
 FROM [BikeStores].[sales].[order_items]
 
-SELECT 
-	 count(*) as row_count
+SELECT count(*) as row_count
   FROM [BikeStores].[sales].[order_items]
+
+Inner Join
 
 SELECT product_name, model_year, brand_name, category_name
   FROM [BikeStores].[production].[products] as p
@@ -25,3 +26,18 @@ SELECT product_name, model_year, brand_name, category_name
   on p.brand_id = b.brand_id
   inner join [BikeStores].[production].categories as c
   on p.category_id  = c.category_id
+
+Left Outer Join
+
+SELECT *
+  FROM [BikeStores].[sales].[stores] as s
+  left outer join  [BikeStores].[sales].staffs as st
+  on s.store_id = st.store_id
+
+
+Right 
+
+SELECT *
+  FROM [BikeStores].[sales].[staffs] s
+  right outer join [BikeStores].[sales].stores st
+  on s.store_id = st.store_id
